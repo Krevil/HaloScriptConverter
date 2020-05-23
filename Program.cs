93da1864901f7aab24c7538f6c401e7cf260b17b
Krevil <Krevil.3000@gmail.com>
2020-05-23 19:32:58 +0100

    Altered comments, made file more presentable

--- Program.cs
+++ Program.cs
@@ -11,45 +11,42 @@ namespace HaloScriptConverter
         {
             try
             {
-                string string1 = args[0];
+                string string1 = args[0]; //So it uses the args like all the big boy programs
                 string string2 = args[1];
-                //string string1 = Console.ReadLine();
-                //string string2 = Console.ReadLine();
                 byte[] File1 = File.ReadAllBytes(string1);
                 Console.WriteLine(File1.Length);
-                List<byte> File1List = File1.ToList();
+                List<byte> File1List = File1.ToList(); //Convert to list because removing elements from arrays is yucky
                 Console.WriteLine(File1List.Capacity);
                 int File1ListCapacity = File1List.Capacity;
-                for (int i = 0; i < File1ListCapacity; i = i + 23) //You need to take into account the size of the file as it's having objects removed
+                for (int i = 0; i < File1ListCapacity; i = i + 23) //Loop through the file, del first byte 
                 {
                     {
                         File1List.RemoveAt(i);
-                        File1ListCapacity = File1ListCapacity-1;
+                        File1ListCapacity = File1ListCapacity-1; //The list is now one element less, so it doesn't try to go over capacity do this
                     }
                 }
-                for (int i = 0; i < File1ListCapacity; i = i + 22) //You need to take into account the size of the file as it's having objects removed
-                {
-                    {
-                        File1List.RemoveAt(i);
-                        File1ListCapacity = File1ListCapacity - 1;
-                    }
-                }
-                for (int i = 0; i < File1ListCapacity; i = i + 21) //You need to take into account the size of the file as it's having objects removed
+                for (int i = 0; i < File1ListCapacity; i = i + 22) //Repeat the loops four times because there's four bytes that need to go
                 {
                     {
                         File1List.RemoveAt(i);
                         File1ListCapacity = File1ListCapacity - 1;
                     }
                 }
-                for (int i = 0; i < File1ListCapacity; i = i + 20) //You need to take into account the size of the file as it's having objects removed
+                for (int i = 0; i < File1ListCapacity; i = i + 21)
                 {
                     {
                         File1List.RemoveAt(i);
                         File1ListCapacity = File1ListCapacity - 1;
                     }
                 }
-                byte[] File1Array = File1List.ToArray();
-                //Console.WriteLine(File1Array[20]);
+                for (int i = 0; i < File1ListCapacity; i = i + 20)
+                {
+                    {
+                        File1List.RemoveAt(i);
+                        File1ListCapacity = File1ListCapacity - 1;
+                    }
+                }
+                byte[] File1Array = File1List.ToArray(); //Back to an array so we can write it to file
                 File.WriteAllBytes(string2, File1Array);
 
             }
