a4db502a76a73d83486d2cf44de16d23aa885f3c
Krevil <Krevil.3000@gmail.com>
2020-05-25 14:17:42 +0100

    Removed useless writelines, added success writeline

--- Program.cs
+++ Program.cs
@@ -14,9 +14,7 @@ namespace HaloScriptConverter
                 string string1 = args[0]; //So it uses the args like all the big boy programs
                 string string2 = args[1];
                 byte[] File1 = File.ReadAllBytes(string1);
-                Console.WriteLine(File1.Length);
                 List<byte> File1List = File1.ToList(); //Convert to list because removing elements from arrays is yucky
-                Console.WriteLine(File1List.Capacity);
                 int File1ListCapacity = File1List.Capacity;
                 for (int i = 20; i < File1ListCapacity; i = i + 23) //Loop through the file, del first byte 
                 {
@@ -48,6 +46,7 @@ namespace HaloScriptConverter
                 }
                 byte[] File1Array = File1List.ToArray(); //Back to an array so we can write it to file
                 File.WriteAllBytes(string2, File1Array);
+                Console.WriteLine("Operation completed successfully.");
 
             }
             catch
